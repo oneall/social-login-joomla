2ea0985b5c5007d75d7c3b48c77748319cdb5a35
Damien <dzara@oneall.com>
2017-10-19 15:17:56 +0200

    CSS class names edited

--- upload/admin/views/sociallogin/tmpl/default.php
+++ upload/admin/views/sociallogin/tmpl/default.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package   	SocialLogin
- * @copyright 	Copyright 2012 http://www.oneall.com - All rights reserved.
- * @license   	GNU/GPL 2 or later
+ * @package       SocialLogin
+ * @copyright     Copyright 2012 http://www.oneall.com - All rights reserved.
+ * @license       GNU/GPL 2 or later
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,16 +22,16 @@
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  */
-defined ('_JEXEC') or die ('Direct Access to this location is not allowed.');
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 JHtml::_('behavior.tooltip');
 
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_sociallogin&view=sociallogin&layout=default'); ?>" method="post" name="adminForm">
 	<fieldset class="social_login_form social_login_form_welcome">
 		<?php
-			if ( empty ($this->settings ['api_settings_verified']))
-			{
-				?>
+if (empty($this->settings['api_settings_verified']))
+{
+    ?>
 					<div class="row row_title">
 						Make your Joomla Portal social!
 					</div>
@@ -54,10 +54,10 @@ JHtml::_('behavior.tooltip');
 						</div>
 					</div>
 				<?php
-			}
-			else
-			{
-				?>
+}
+else
+{
+    ?>
 					<div class="row row_title">
 							Your API Account is setup correctly
 					</div>
@@ -73,11 +73,11 @@ JHtml::_('behavior.tooltip');
 						</div>
 					</div>
 				<?php
-			}
-		?>
+}
+?>
 	</fieldset>
 	<fieldset class="social_login_form social_login_form_info">
-		<legend><?php echo JText::_('Help, Updates &amp; Documentation');?></legend>
+		<legend><?php echo JText::_('Help, Updates &amp; Documentation'); ?></legend>
 		<div class="row">
 			<ul>
 				<li>
@@ -101,25 +101,25 @@ JHtml::_('behavior.tooltip');
 		</div>
 		<div class="row row_odd">
 			<?php
-				$api_use_curl = (! isset ($this->settings ['api_use_curl']) OR !empty($this->settings ['api_use_curl']));
-			?>
+$api_use_curl = (!isset($this->settings['api_use_curl']) or !empty($this->settings['api_use_curl']));
+?>
 			<input id="api_use_curl_yes" type="radio" name="settings[api_use_curl]" value="1" <?php echo ($api_use_curl ? 'checked="checked"' : ''); ?> />
 			<label for="api_use_curl_yes" style="clear:none">Use CURL to communicate with the API <strong>(Recommended)</strong></label>
 			<div class="clr"></div>
-			<input id="api_use_curl_no" type="radio" name="settings[api_use_curl]" value="0" <?php echo ( ! $api_use_curl ? 'checked="checked"' : ''); ?> />
+			<input id="api_use_curl_no" type="radio" name="settings[api_use_curl]" value="0" <?php echo (!$api_use_curl ? 'checked="checked"' : ''); ?> />
 			<label for="api_use_curl_no" style="clear:none">Use FSOCKOPEN to communicate with the API</label>
 		</div>
 		<div class="row row_even">
 			<label for="oneall_api_subdomain"  style="width: 200px;">API Subdomain:</label>
-			<input type="text" id="settings_api_subdomain" name="settings[api_subdomain]" size="60" value="<?php echo (isset ($this->settings ['api_subdomain']) ? htmlspecialchars ($this->settings ['api_subdomain']) : ''); ?>" />
+			<input type="text" id="settings_api_subdomain" name="settings[api_subdomain]" size="60" value="<?php echo (isset($this->settings['api_subdomain']) ? htmlspecialchars($this->settings['api_subdomain']) : ''); ?>" />
 		</div>
 		<div class="row row_even">
 			<label for="oneall_api_public_key" style="width: 200px;">API Public Key:</label>
-			<input type="text" id="settings_api_key" name="settings[api_key]" size="60" value="<?php echo (isset ($this->settings ['api_key']) ? htmlspecialchars ($this->settings ['api_key']) : ''); ?>" />
+			<input type="text" id="settings_api_key" name="settings[api_key]" size="60" value="<?php echo (isset($this->settings['api_key']) ? htmlspecialchars($this->settings['api_key']) : ''); ?>" />
 		</div>
 		<div class="row row_even">
 			<label for="oneall_api_private_key" style="width: 200px;">API Private Key:</label>
-			<input type="text" id="settings_api_secret"  name="settings[api_secret]" size="60" value="<?php echo (isset ($this->settings ['api_secret']) ? htmlspecialchars ($this->settings ['api_secret']) : ''); ?>" />
+			<input type="text" id="settings_api_secret"  name="settings[api_secret]" size="60" value="<?php echo (isset($this->settings['api_secret']) ? htmlspecialchars($this->settings['api_secret']) : ''); ?>" />
 		</div>
 		<div class="row row_button">
 			<div class="button2-left">
@@ -133,19 +133,19 @@ JHtml::_('behavior.tooltip');
 	<fieldset class="social_login_form">
 		<legend>Enable the social networks/identity providers of your choice</legend>
 			<?php
-				$i = 0;
-				foreach ($this->providers AS $key => $provider_data)
-				{
-					?>
+$i = 0;
+foreach ($this->providers as $key => $provider_data)
+{
+    ?>
 						<div class="row <?php echo ((($i++) % 2) == 0) ? 'row_even' : 'row_odd' ?> row_provider">
-							<label class="provider_icon" for="oneall_social_login_provider_<?php echo $key; ?>"><span class="oa_provider oa_provider_<?php echo $key; ?>" title="<?php echo htmlspecialchars ($provider_data['name']); ?>"><?php echo htmlspecialchars ($provider_data['name']); ?></span></label>
-							<input class="provider_check" type="checkbox" id="oneall_social_login_provider_<?php echo $key; ?>" name="settings[providers][<?php echo $key; ?>]" value="1" <?php echo (in_array($key, $this->settings ['providers']) ? 'checked="checked"' : ''); ?> />
-							<label class="provider_name" for="oneall_social_login_provider_<?php echo $key; ?>"><?php echo htmlspecialchars ($provider_data['name']); ?></label>
+							<label class="provider_icon" for="oneall_social_login_provider_<?php echo $key; ?>"><span class="oa_provider oa_social_login_provider_<?php echo $key; ?>" title="<?php echo htmlspecialchars($provider_data['name']); ?>"><?php echo htmlspecialchars($provider_data['name']); ?></span></label>
+							<input class="provider_check" type="checkbox" id="oneall_social_login_provider_<?php echo $key; ?>" name="settings[providers][<?php echo $key; ?>]" value="1" <?php echo (in_array($key, $this->settings['providers']) ? 'checked="checked"' : ''); ?> />
+							<label class="provider_name" for="oneall_social_login_provider_<?php echo $key; ?>"><?php echo htmlspecialchars($provider_data['name']); ?></label>
 							<div class="clr"></div>
 						</div>
 					<?php
-			}
-		?>
+}
+?>
 	</fieldset>
 	<fieldset class="social_login_form">
 		<legend>Settings</legend>
@@ -153,19 +153,19 @@ JHtml::_('behavior.tooltip');
 				Enter the caption to be displayed above the social network login buttons:
 			</div>
 			<div class="row row_even">
-				<input type="text" name="settings[mod_caption]" size="118" value="<?php echo (isset ($this->settings ['mod_caption']) ? htmlspecialchars ($this->settings ['mod_caption']) : 'Connect with:'); ?>" />
+				<input type="text" name="settings[mod_caption]" size="118" value="<?php echo (isset($this->settings['mod_caption']) ? htmlspecialchars($this->settings['mod_caption']) : 'Connect with:'); ?>" />
 			</div>
 			<div class="row row_odd">
 				Should social network profiles with verified email addresses be linked to existing account?
 			</div>
 			<div class="row row_odd">
 			<?php
-				$link_verified_accounts = (! isset ($this->settings ['link_verified_accounts']) OR !empty($this->settings ['link_verified_accounts']));
-			?>
+$link_verified_accounts = (!isset($this->settings['link_verified_accounts']) or !empty($this->settings['link_verified_accounts']));
+?>
 			<input id="link_verified_accounts_yes" type="radio" name="settings[link_verified_accounts]" value="1" <?php echo ($link_verified_accounts ? 'checked="checked"' : ''); ?> />
 			<label for="link_verified_accounts_yes" style="clear:none">Yes, try to link verified social network profiles to existing accounts <strong>(Default)</strong></label>
 			<div class="clr"></div>
-			<input id="link_verified_accounts_no" type="radio" name="settings[link_verified_accounts]" value="0" <?php echo ( ! $link_verified_accounts ? 'checked="checked"' : ''); ?> />
+			<input id="link_verified_accounts_no" type="radio" name="settings[link_verified_accounts]" value="0" <?php echo (!$link_verified_accounts ? 'checked="checked"' : ''); ?> />
 			<label for="$link_verified_accounts_no" style="clear:none">No, disable account linking </label>
 		</div>
 	</fieldset>
