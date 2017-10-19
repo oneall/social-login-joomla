9169f9d0430e98502a9068fc8c3fbc5090887d2d
Damien <dzara@oneall.com>
2017-10-19 12:00:18 +0200

    Social Login 1.1.122 for Joomla 1.5+

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 An installation guide is available here:
 http://docs.oneall.com/plugins/guide/social-login-joomla/
+
+
+## Available Versions
+* **Social Login for Joomla 1.5+**
+
+
+# Social Login 1.1.122 for Joomla 1.5+
+
+Social Login for Joomla allows your users to login and register with 35+ social networks. 
+It increases your Joomla user registration rate by simplifying the registration process for 
+new users and provides permission-based social data retrieved from the social network profiles.
+
+
+* [Social Login Installation guide](http://docs.oneall.com/plugins/guide/social-login-joomla/)
+
+## Supported Social Networks
+* Facebook
+* Twitter
+* Google
+* LinkedIn
+* Yahoo
+* Github.com
+* Foursquare
+* YouTube
+* Skyrock.com		
+* OpenID
+* WordPress.com
+* PayPal
+* LiveJournal
+* Steam
+* Windows Live
+* Blogger
+* Disqus
+* StackExchange
+* VKontakte
+* Odnoklassniki
+* Mail.ru
+
+
+## About Us
+Social Login has been created and is maintained by [OneAll](http://www.oneall.com/), a technology company offering a set of 
+web-delivered tools and services for establishing and optimizing a site's connection with social networks.
+
+
+## License
+The **GNU General Public License** (GPL) is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+
+
+
+**Fork us on Github!**
---
+++ upload/admin/assets/cfg.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @package       SocialLogin
+ * @copyright     Copyright 2012 http://www.oneall.com - All rights reserved.
+ * @license       GNU/GPL 2 or later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ *
+ * The "GNU General Public License" (GPL) is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ */
+
+/**
+ * Supported Providers
+ */
+
+$social_login_providers = array(
+    'facebook' => array(
+        'name' => 'Facebook'
+    ),
+    'twitter' => array(
+        'name' => 'Twitter'
+    ),
+    'google' => array(
+        'name' => 'Google'
+    ),
+    'linkedin' => array(
+        'name' => 'LinkedIn'
+    ),
+    'yahoo' => array(
+        'name' => 'Yahoo'
+    ),
+    'github' => array(
+        'name' => 'Github.com'
+    ),
+    'foursquare' => array(
+        'name' => 'Foursquare'
+    ),
+    'youtube' => array(
+        'name' => 'YouTube'
+    ),
+    'skyrock' => array(
+        'name' => 'Skyrock.com'
+    ),
+    'openid' => array(
+        'name' => 'OpenID'
+    ),
+    'wordpress' => array(
+        'name' => 'Wordpress.com'
+    ),
+    'paypal' => array(
+        'name' => 'PayPal'
+    ),
+    'livejournal' => array(
+        'name' => 'LiveJournal'
+    ),
+    'steam' => array(
+        'name' => 'Steam Community'
+    ),
+    'windowslive' => array(
+        'name' => 'Windows Live'
+    ),
+    'blogger' => array(
+        'name' => 'Blogger'
+    ),
+    'disqus' => array(
+        'name' => 'Disqus'
+    ),
+    'stackexchange' => array(
+        'name' => 'StackExchange'
+    ),
+    'vkontakte' => array(
+        'name' => 'VKontakte (Вконтакте)'
+    ),
+    'odnoklassniki' => array(
+        'name' => 'Odnoklassniki.ru'
+    ),
+    'mailru' => array(
+        'name' => 'Mail.ru'
+    )
+);
